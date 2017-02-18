45957dcc245793ee2b9489caba1a13c5caa63ee1
koharker <koharker@gmail.com>
2017-02-18 14:05:38 -0800

    Update band.js

--- band.js
+++ band.js
@@ -265,7 +265,7 @@ function drawChairXY(x, y, t, n, a, chair) {
 				strokeStyle: '#000',
 				strokeWidth: 5,
 				width: 40 * seatScale, height: 10 * seatScale,
-				x: x + Math.sin(t) * 12, y: y + Math.cos(t) * 12,
+				x: x - Math.sin(t) * 12, y: y - Math.cos(t) * 12,
 				angle: -1 * t
 			});
 			$('canvas').drawEllipse({
