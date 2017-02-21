2ee9f5dd8c2d2d6a474ed5960affaa4d67f7057e
koharker <koharker@gmail.com>
2017-02-21 09:49:39 -0800

    Update band.js

--- band.js
+++ band.js
@@ -143,7 +143,7 @@ function drawChart() {
 							console.log(vc);
 							var t = -1 * (-1 * arc_length / 2 + angle_step * 1.1 * i);
 						} else {
-							var t = -1 * (-1 * arc_length / 2 + angle_step * 0.9 * i);
+							var t = -1 * (-1 * arc_length / 2 + (angle_step + (angle_step * 0.1 * vc)/(rows.length - 1)) * i);
 							/*for (var j = i; j >= 0; j--) {
 								var vt = -1 * (-1 * arc_length / 2 + (angle_step * 0.9) * j);  //(1 - (0.1 * vc)/(rows.length - 1)))
 								drawChair(r, vt, n, a, chairs[row][j]);
