5841d1bc8062864df69d10d40b0bfe96e8136ba9
koharker <koharker@gmail.com>
2017-02-21 14:44:15 -0800

    Update band.js

--- band.js
+++ band.js
@@ -141,17 +141,17 @@ function drawChart() {
 					//adjust for cello spacing here
 					if (vc > 0) {
 						if (chairs[row][i].shape === "cello") {
-							var vcLoc = i;
+							vcLoc = i;
 							console.log(vcLoc + "vcLoc");
 							var t = -1 * (-1 * arc_length / 2 + vcStep * i);
 						/*} else if (rows[row] > vcLoc) {
 							var t = -1 * (-1 * arc_length / 2 + ((angle_step - (vcStep / rows.length))* i) + (vcStep * vcLoc));
 						*/ 
 						} else if (i > vcLoc) {
-							console.log(i + "chair< " + vcLoc + "vcLoc")
+							console.log(i + "chair " + vcLoc + "vcLoc")
 							var t = -1 * (-1 * arc_length / 2 + ((angle_step - (angle_step * 0.1 / rows.length))* i) + vcStep);
-						} else if (i < vcLoc) {
-							console.log(i + "chair < " + vcLoc + "vcLoc")
+						} else {
+							console.log(i + "chair '<' " + vcLoc + "vcLoc")
 							var t = -1 * (-1 * arc_length / 2 + ((angle_step - (angle_step * 0.1 / rows.length))* i));
 							/*for (var j = i; j >= 0; j--) {
 								var vt = -1 * (-1 * arc_length / 2 + (angle_step * 0.9) * j);  //(1 - (0.1 * vc)/(rows.length - 1)))
