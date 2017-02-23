c0e0ad263f832af14eba7ed58ee8a3676e77d1bc
koharker <koharker@gmail.com>
2017-02-23 11:51:23 -0800

    Update band.js

--- band.js
+++ band.js
@@ -318,7 +318,7 @@ function drawChairXY(x, y, t, n, a, chair) {
 				strokeStyle: '#000',
 				strokeWidth: 5,
 				width: 38 * seatScale, height: 10 * seatScale,
-				x: (x + Math.sin(t) * 9) * seatScale, y: y + Math.cos(t) * 9 * seatScale,
+				x: x + Math.sin(t) * 9 * seatScale, y: y + Math.cos(t) * 9 * seatScale,
 				angle: -1 * t
 			});
 			$('canvas').drawEllipse({
@@ -365,7 +365,7 @@ function drawChairXY(x, y, t, n, a, chair) {
 				strokeStyle: '#000',
 				strokeWidth: 5,
 				width: 10 * seatScale, height: 38 * seatScale,
-				x: (x + Math.sin(t+19)) * seatScale, y: (y + Math.cos(t+19)) * seatScale,
+				x: x + Math.sin(t + (18 * seatScale)), y: y + Math.cos(t + (18 * seatScale)),
 				angle: -1 * t
 			});
 			$('canvas').drawEllipse({
@@ -373,7 +373,7 @@ function drawChairXY(x, y, t, n, a, chair) {
 				strokeStyle: '#fff',
 				strokeWidth: 5,
 				width: 10 * seatScale - 4, height: 38 * seatScale - 4 ,
-				x: (x + Math.sin(t+19)) * seatScale, y: (y + Math.cos(t+19)) * seatScale,
+				x: x + Math.sin(t + (18 * seatScale)), y: y + Math.cos(t + (18 * seatScale)),
 				angle: -1 * t
 			});
 			$('canvas').drawRect({
@@ -395,7 +395,7 @@ function drawChairXY(x, y, t, n, a, chair) {
 				strokeStyle: '#000',
 				strokeWidth: 5,
 				width: 10 * seatScale, height: 38 * seatScale,
-				x: (x - 19) * seatScale, y: y * seatScale,
+				x: x + Math.sin(t - (18 * seatScale)), y: y + Math.cos(t - (18 * seatScale)),
 				angle: -1 * t
 			});
 			$('canvas').drawEllipse({
@@ -403,7 +403,7 @@ function drawChairXY(x, y, t, n, a, chair) {
 				strokeStyle: '#fff',
 				strokeWidth: 5,
 				width: 10 * seatScale - 4, height: 38 * seatScale - 4 ,
-				x: (x - 19) * seatScale, y: y * seatScale,
+				x: x + Math.sin(t - (18 * seatScale)), y: y + Math.cos(t - (18 * seatScale)),
 				angle: -1 * t
 			});
 		}
